09214236dbdf83a27336b949bb60198fe1d0a008
JoaoMatheusDev <63765886+JoaoMatheusDev@users.noreply.github.com>
2021-03-02 20:21:22 -0300

    SiteP2

---
+++ P2SiteMorais/js/Form.js
@@ -0,0 +1,14 @@
+function SalvaForm(){
+      Swal.fire({
+        title: 'Parabéns, Seu Carro foi Alugado.',
+        width: 600,
+        padding: '3em',
+        background: '#fff url(/images/trees.png)',
+        backdrop: `
+          rgba(0,0,123,0.4)
+          url("fotos/nyan.gif")
+          left top
+          no-repeat
+        `
+      })
+   }
